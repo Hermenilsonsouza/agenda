2a07b6dfc37998d28abc395ff017fe0e2ed2e34d
Hermenilsonsouza <your_github_email>
2023-11-06 09:27:22 -0300

    add fields and models

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from contact.models import Contact
+from contact.models import Contact, Category
 
 # Register your models here.
 @admin.register(Contact)
@@ -9,3 +9,10 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 100
+    list_display_links = 'id', 'first_name',
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
